7db6c7304aa1b576c9fd003f38d0170bd982305f
Tomasz <tomaszkaramon3@gmail.com>
2024-02-18 02:42:49 +0100

    Added keycloak authorization and authentication for all endpoints

--- movie-command-service/src/main/java/com/moviecommandservice/MovieCommandServiceApplication.java
+++ movie-command-service/src/main/java/com/moviecommandservice/MovieCommandServiceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MovieCommandServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCommandServiceApplication.class, args);
 	}
-
 }
--- movie-command-service/src/main/java/com/moviecommandservice/controller/MovieCommandController.java
+++ movie-command-service/src/main/java/com/moviecommandservice/controller/MovieCommandController.java
@@ -4,27 +4,32 @@ import com.moviecommandservice.dto.MovieEvent;
 import com.moviecommandservice.entity.Movie;
 import com.moviecommandservice.exceptions.MovieNotFoundException;
 import com.moviecommandservice.service.MovieCommandService;
+import jakarta.annotation.security.RolesAllowed;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
-
+@RestController
 @Controller
 @RequestMapping("/movies")
 public class MovieCommandController {
     @Autowired
     private MovieCommandService commandService;
     @PostMapping
+    @PreAuthorize("hasRole('client_admin')")
     public ResponseEntity<String> createMovie(@RequestBody MovieEvent movieEvent)
     {
         Movie newMovie = commandService.createMovie(movieEvent);
         return new ResponseEntity<String>("Movie was created "+movieEvent.getMovie().getId(), HttpStatus.OK);
     }
 
+
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('client_admin')")
     public ResponseEntity<String> updateMovie(@PathVariable UUID id,@RequestBody MovieEvent updatedMovie){
         try{
             commandService.updateMovie(id,updatedMovie);
@@ -35,6 +40,7 @@ public class MovieCommandController {
         }
     }
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('client_admin')")
     public ResponseEntity<String> deleteMovie(@PathVariable UUID id){
         try{
             commandService.deleteMovie(id);
